0918d0f047b3af3aa2075d87a3ea94eecc21847e
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-08-28 10:42:04 +0300

    Update main.tf

--- server/main.tf
+++ server/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-6871a115"
+  ami           = "ami-04169656fea786776"
   instance_type = "t2.micro"
   key_name      = "${aws_key_pair.chavo.id}"
 
